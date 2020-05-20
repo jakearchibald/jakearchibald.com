ca269dabbbd00e30dee22be52466e40138abf4e1
Jake Archibald <jaffathecake@gmail.com>
2020-05-20 11:17:10 +0100

    Adding feed

--- static-build/index.tsx
+++ static-build/index.tsx
@@ -11,18 +11,14 @@
  * limitations under the License.
  */
 import { h } from 'preact';
+import { Feed } from 'feed';
 
-import { renderPage, writeFiles, getPostPath } from './utils';
+import { renderPage, writeFiles, getPostPath, getPostUrl } from './utils';
 import IndexPage from './pages/index';
 import posts from 'post-data:';
 import WhoPage from './pages/who';
 import PostPage from './pages/post';
-
-const indexPageSize = 10;
-const paginatedPosts = Array.from(
-  { length: Math.ceil(posts.length / indexPageSize) },
-  (_, i) => posts.slice(indexPageSize * i, indexPageSize * i + indexPageSize),
-);
+import iconUrl from 'asset-url:./pages/post/icon.png';
 
 interface Output {
   [outputPath: string]: string;
@@ -30,6 +26,13 @@ interface Output {
 const toOutput: Output = {
   'who/index.html': renderPage(<WhoPage />),
 };
+
+// Paginated index pages
+const indexPageSize = 10;
+const paginatedPosts = Array.from(
+  { length: Math.ceil(posts.length / indexPageSize) },
+  (_, i) => posts.slice(indexPageSize * i, indexPageSize * i + indexPageSize),
+);
 
 for (const [i, posts] of paginatedPosts.entries()) {
   toOutput[i === 0 ? `index.html` : `${i + 1}.html`] = renderPage(
@@ -41,10 +44,35 @@ for (const [i, posts] of paginatedPosts.entries()) {
   );
 }
 
+// Post pages & RSS
+const feed = new Feed({
+  title: `Jake Archibald's blog`,
+  link: `https://jakearchibald.com/`,
+  id: `https://jakearchibald.com/`,
+  language: `en-gb`,
+  image: `https://jakearchibald.com${iconUrl}`,
+  favicon: `https://jakearchibald.com/favicon.ico`,
+  copyright: 'https://creativecommons.org/licenses/by/4.0/',
+});
+
 for (const post of posts) {
   toOutput[getPostPath(post) + 'index.html'] = renderPage(
     <PostPage post={post} />,
   );
+
+  feed.addItem({
+    title: post.title,
+    id: getPostUrl(post),
+    link: `https://jakearchibald.com${getPostUrl(post)}`,
+    content: post.content,
+    description: post.meta,
+    date: new Date(post.date),
+    image: post.image
+      ? `https://jakearchibald.com${post.image}`
+      : `https://jakearchibald.com${iconUrl}`,
+  });
 }
+
+toOutput['posts.rss'] = feed.atom1();
 
 writeFiles(toOutput);
