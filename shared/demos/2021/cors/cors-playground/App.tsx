cba56feb437381ddfeb18b00e1969016d4b49b4e
Jake Archibald <jaffathecake@gmail.com>
2021-10-11 09:26:04 +0100

    Making request

--- shared/demos/2021/cors/cors-playground/App.tsx
+++ shared/demos/2021/cors/cors-playground/App.tsx
@@ -55,16 +55,14 @@ const defaultState: State = {
 function dataToState(data: FormData | URLSearchParams) {
   return {
     requestMethod: (data.get('requestMethod') ?? 'GET').toString(),
-    requestUseCORS: data.has('requestUseCORS'),
-    requestSendCredentials: data.has('requestSendCredentials'),
+    requestUseCORS: Boolean(data.get('requestUseCORS')),
+    requestSendCredentials: Boolean(data.get('requestSendCredentials')),
     requestHeaders: dataToPairs(
       data,
       'requestHeaderName',
       'requestHeaderValue',
     ),
-    preflightStatus: Number(
-      data.get('preflightStatus') ?? defaultState.preflightStatus,
-    ),
+    preflightStatus: Number(data.get('preflightStatus') ?? 206),
     preflightAllowOrigin: (data.get('preflightAllowOrigin') ?? '').toString(),
     preflightAllowCredentials: (
       data.get('preflightAllowCredentials') ?? ''
@@ -131,12 +129,113 @@ export default class App extends Component<Props, State> {
     }));
   };
 
+  private _setURL() {
+    const url = new URL(location.href);
+    url.search = '';
+    url.searchParams.append('prefillForm', '1');
+
+    const fields = [
+      'requestMethod',
+      'requestUseCORS',
+      'requestSendCredentials',
+      'preflightStatus',
+      'preflightAllowOrigin',
+      'preflightAllowCredentials',
+      'preflightAllowMethods',
+      'preflightAllowHeaders',
+      'responseAllowOrigin',
+      'responseAllowCredentials',
+      'responseExposeHeaders',
+    ] as const;
+
+    for (const field of fields) {
+      if (typeof this.state[field] === 'boolean') {
+        url.searchParams.set(field, this.state[field] ? '1' : '');
+      } else {
+        url.searchParams.set(field, this.state[field].toString());
+      }
+    }
+
+    for (const [name, value] of this.state.requestHeaders) {
+      url.searchParams.append('requestHeaderName', name);
+      url.searchParams.append('requestHeaderValue', value);
+    }
+
+    for (const [name, value] of this.state.responseCookies) {
+      url.searchParams.append('responseCookieName', name);
+      url.searchParams.append('responseCookieValue', value);
+    }
+
+    history.replaceState(null, '', url.href);
+  }
+
+  private _abortController?: AbortController;
+  private _queue: Promise<any> = Promise.resolve();
+
+  private _onFormSubmit = (event: Event) => {
+    event.preventDefault();
+    this._setURL();
+
+    if (this._abortController) this._abortController.abort();
+    const controller = new AbortController();
+    this._abortController = controller;
+
+    const { state } = this;
+
+    this._queue = this._queue.finally(async () => {
+      const id = Math.random().toString();
+      const url = new URL('https://cors-playground.deno.dev/resource');
+      const params = url.searchParams;
+
+      params.set('id', id);
+      params.set('preflight-status', state.preflightStatus.toString());
+      params.set(
+        'preflight-access-control-allow-origin',
+        state.preflightAllowOrigin,
+      );
+      params.set(
+        'preflight-access-control-allow-credentials',
+        state.preflightAllowCredentials,
+      );
+      params.set(
+        'preflight-access-control-allow-methods',
+        state.preflightAllowMethods,
+      );
+      params.set(
+        'preflight-access-control-allow-headers',
+        state.preflightAllowHeaders,
+      );
+      params.set('access-control-allow-origin', state.responseAllowOrigin);
+      params.set(
+        'access-control-allow-credentials',
+        state.responseAllowCredentials,
+      );
+      params.set('access-control-expose-headers', state.responseExposeHeaders);
+
+      for (const [name, value] of state.responseCookies) {
+        params.append('cookie-name', name);
+        params.append('cookie-value', value);
+      }
+
+      const request = new Request(url.href, {
+        method: state.requestMethod,
+        mode: state.requestUseCORS ? 'cors' : 'no-cors',
+        credentials: state.requestSendCredentials ? 'include' : 'omit',
+        headers: state.requestHeaders,
+        signal: controller.signal,
+      });
+
+      const response = await fetch(request);
+      console.log(response);
+    });
+  };
+
   render(_: Props, state: State) {
     return (
       <div class="app">
         <header></header>
         <main>
-          <form ref={this._formRef}>
+          <form ref={this._formRef} onSubmit={this._onFormSubmit}>
             <h2>Request</h2>
             <div class="field-grid" style="--min-cell-width: 250px">
               <TextField
@@ -157,11 +256,16 @@ export default class App extends Component<Props, State> {
                 state={state}
                 onInput={this._onInput}
               />
-              {state.requestHeaders.map(([name, value], i) => (
-                <Fragment key={i}>
+            </div>
+            {state.requestHeaders.map(([name, value], i) => (
+              <Fragment key={i}>
+                <div class="divider">
+                  <h3>Header</h3>
+                </div>
+                <div class="field-grid" style="--min-cell-width: 250px">
                   <div class="field new-row">
                     <MaterialText
-                      label="Header name"
+                      label="Name"
                       input={{
                         name: 'requestHeaderName',
                         value: name,
@@ -171,7 +275,7 @@ export default class App extends Component<Props, State> {
                   </div>
                   <div class="field">
                     <MaterialText
-                      label="Header value"
+                      label="Value"
                       input={{
                         name: 'requestHeaderValue',
                         value: value,
@@ -186,12 +290,15 @@ export default class App extends Component<Props, State> {
                         data-index={i}
                         onClick={this._onRemoveHeaderClick}
                       >
-                        Remove header
+                        Remove
                       </button>
                     </div>
                   </div>
-                </Fragment>
-              ))}
+                </div>
+              </Fragment>
+            ))}
+            <div class="divider"></div>
+            <div class="field-grid" style="--min-cell-width: 250px">
               <div class="field new-row">
                 <div class="button-field">
                   <button type="button" onClick={this._onAddHeaderClick}>
@@ -253,11 +360,17 @@ export default class App extends Component<Props, State> {
                 state={state}
                 onInput={this._onInput}
               />
-              {state.responseCookies.map(([name, value], i) => (
-                <Fragment key={i}>
+            </div>
+
+            {state.responseCookies.map(([name, value], i) => (
+              <Fragment key={i}>
+                <div class="divider">
+                  <h3>Cookie</h3>
+                </div>
+                <div class="field-grid" style="--min-cell-width: 250px">
                   <div class="field new-row">
                     <MaterialText
-                      label="Cookie name"
+                      label="Name"
                       input={{
                         name: 'responseCookieName',
                         value: name,
@@ -267,7 +380,7 @@ export default class App extends Component<Props, State> {
                   </div>
                   <div class="field">
                     <MaterialText
-                      label="Cookie value"
+                      label="Value"
                       input={{
                         name: 'responseCookieValue',
                         value: value,
@@ -282,18 +395,27 @@ export default class App extends Component<Props, State> {
                         data-index={i}
                         onClick={this._onRemoveCookieClick}
                       >
-                        Remove cookie
+                        Remove
                       </button>
                     </div>
                   </div>
-                </Fragment>
-              ))}
+                </div>
+              </Fragment>
+            ))}
+            <div class="divider"></div>
+            <div class="field-grid" style="--min-cell-width: 250px">
               <div class="field new-row">
                 <div class="button-field">
                   <button type="button" onClick={this._onAddCookieClick}>
                     Add cookie
                   </button>
                 </div>
+              </div>
+            </div>
+            <div class="divider"></div>
+            <div class="field-grid" style="--min-cell-width: 250px">
+              <div class="button-field">
+                <button>Make request</button>
               </div>
             </div>
           </form>
@@ -333,6 +455,7 @@ const CheckboxField: FunctionalComponent<FieldProps> = (props) => (
           name: props.name,
           checked: !!props.state[props.name],
           onChange: props.onInput,
+          value: '1',
         }}
       />{' '}
       {props.label}
