8c0954546ed2ca24b7aa1194d4cdd449d36de864
jakearchibald <jaffathecake@gmail.com>
2022-07-11 14:16:17 +0100

    Aspect ratio article

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,7 @@ function resolveFileUrl({ fileName }) {
 
 const staticPath = 'static/c/[name]-[hash][extname]';
 const jsPath = staticPath.replace('[extname]', '.js');
-const limitPostsDevBuild = 'static-build/posts/2021/11';
+const limitPostsDevBuild = 'static-build/posts/2022/07';
 
 function jsFileName(chunkInfo) {
   if (!chunkInfo.facadeModuleId) return jsPath;
