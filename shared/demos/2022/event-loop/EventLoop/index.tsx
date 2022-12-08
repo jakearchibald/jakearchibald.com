34c627ba0bdfcdb0dc211d842e34c3ce6ec32a40
jakearchibald <jaffathecake@gmail.com>
2022-12-08 17:03:06 +0000

    Use opacity for other paths

--- shared/demos/2022/event-loop/EventLoop/index.tsx
+++ shared/demos/2022/event-loop/EventLoop/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionalComponent, h } from 'preact';
-import { useSignal } from '@preact/signals';
+import { computed, useSignal } from '@preact/signals';
 
 import { escapeStyleScriptContent } from 'shared/utils';
 import { inline as styles } from 'css-bundle:./styles.css';
@@ -37,8 +37,12 @@ const EventLoop: FunctionalComponent<Props> = ({
   const showRenderPath = useSignal(initialState?.showRenderPath ?? false);
   const mode = useSignal(initialState?.mode ?? 'default');
 
+  const taskPathOpacity = computed(() => (showTaskPath.value ? '1' : '0'));
+  const renderPathOpacity = computed(() => (showRenderPath.value ? '1' : '0'));
+
   return (
     <div class="event-loop-container">
+      {renderPathOpacity}
       <div
         class="event-loop-cropper"
         style={{ '--width': width, '--height': height }}
@@ -50,31 +54,29 @@ const EventLoop: FunctionalComponent<Props> = ({
           <g class="event-loop-canvas">
             <g class="path-tracks">
               <path d={circlePath(arcRadius)} />
-              {showTaskPath.value && (
-                <path
-                  d={`M 0 ${arcRadius} l ${-detourOffset} 0 ${circularArcPath({
-                    midX: -detourOffset,
-                    radius: arcRadius,
-                    startAngle: 180,
-                    endAngle: 270 - taskSize / 2,
-                  })} ${circularArcPath({
-                    midX: -detourOffset,
-                    radius: arcRadius,
-                    startAngle: 270 + taskSize / 2,
-                    endAngle: 0,
-                  })} M ${-detourOffset} ${-arcRadius} l ${detourOffset} 0`}
-                />
-              )}
-              {showRenderPath.value && (
-                <path
-                  d={`M 0 ${-arcRadius} l ${detourOffset} 0 ${circularArcPath({
-                    midX: detourOffset,
-                    radius: arcRadius,
-                    startAngle: 0,
-                    endAngle: 180,
-                  })} M ${detourOffset} ${arcRadius} l ${-detourOffset} 0`}
-                />
-              )}
+              <path
+                style={{ opacity: taskPathOpacity.value }}
+                d={`M 0 ${arcRadius} l ${-detourOffset} 0 ${circularArcPath({
+                  midX: -detourOffset,
+                  radius: arcRadius,
+                  startAngle: 180,
+                  endAngle: 270 - taskSize / 2,
+                })} ${circularArcPath({
+                  midX: -detourOffset,
+                  radius: arcRadius,
+                  startAngle: 270 + taskSize / 2,
+                  endAngle: 0,
+                })} M ${-detourOffset} ${-arcRadius} l ${detourOffset} 0`}
+              />
+              <path
+                style={{ opacity: renderPathOpacity.value }}
+                d={`M 0 ${-arcRadius} l ${detourOffset} 0 ${circularArcPath({
+                  midX: detourOffset,
+                  radius: arcRadius,
+                  startAngle: 0,
+                  endAngle: 180,
+                })} M ${detourOffset} ${arcRadius} l ${-detourOffset} 0`}
+              />
             </g>
             {mode.value === 'speedy-spin' && !__PRERENDER__ && <SpeedyLoop />}
           </g>
