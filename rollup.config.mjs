098e7a218945a407ca1645df098caa7820c8097e
Jake Archibald <jake.archibald@shopify.com>
2024-08-05 11:59:16 +0100

    Draft complete

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -41,7 +41,7 @@ function resolveFileUrl({ fileName }) {
 
 const staticPath = 'static/c/[name]-[hash][extname]';
 const jsPath = staticPath.replace('[extname]', '.js');
-const limitPostsDevBuild = 'static-build/posts/2023/07';
+const limitPostsDevBuild = 'static-build/posts/2024/08';
 
 function jsFileName(chunkInfo) {
   if (!chunkInfo.facadeModuleId) return jsPath;
