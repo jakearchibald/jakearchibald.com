ea45c8e753df654e115c5ade54e8c808d5bb7781
Jake Archibald <jaffathecake@gmail.com>
2026-06-09 15:07:43 +0100

    Update picture of me

--- static-build/components/who/index.tsx
+++ static-build/components/who/index.tsx
@@ -1,7 +1,5 @@
 import { h, FunctionalComponent } from 'preact';
 
-import meJpeg from 'asset-url:./me-cat.jpg';
-import meWebp from 'asset-url:./me-cat.webp';
 import meAvif from 'asset-url:./me-cat.avif';
 import twitterUrl from 'asset-url:./twitter.svg';
 import githubUrl from 'asset-url:./github.svg';
@@ -15,16 +13,12 @@ const Who: FunctionalComponent<{}> = () => (
   <section>
     <figure class="my-big-face">
       <h1>
-        <picture>
-          <source srcset={meAvif} type="image/avif" />
-          <source srcset={meWebp} type="image/webp" />
-          <img
-            width="800"
-            height="844"
-            src={meJpeg}
-            alt="Jake Archibald in a garden with a black cat"
-          />
-        </picture>
+        <img
+          width="800"
+          height="907"
+          src={meAvif}
+          alt="Jake Archibald with a black cat on his shoulders"
+        />
       </h1>
     </figure>
 
