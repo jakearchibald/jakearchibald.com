cc5813df480601b935028046a2a5f1fe55a9dca1
Jake Archibald <jaffathecake@gmail.com>
2026-07-26 11:54:19 +0100

    a11y tooltips article

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -41,7 +41,7 @@ function resolveFileUrl({ fileName }) {
 
 const staticPath = 'static/c/[name]-[hash][extname]';
 const jsPath = staticPath.replace('[extname]', '.js');
-const limitPostsDevBuild = 'static-build/posts/2026/06';
+const limitPostsDevBuild = 'static-build/posts/2026/07';
 
 function jsFileName(chunkInfo) {
   if (!chunkInfo.facadeModuleId) return jsPath;
